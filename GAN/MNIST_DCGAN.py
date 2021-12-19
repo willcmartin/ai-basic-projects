5a6e9d0e6bfc8494ca4465efeda90d8b3b9827f5
Will Martin <wcmartin01@gmail.com>
2021-12-19 14:52:05 -0600

    rename file structure

--- GAN/MNIST_DCGAN.py
+++ GAN/MNIST_DCGAN.py
@@ -96,9 +96,89 @@ print("\nGenerator Model:")
 print(summary(generator_model, (1, 100)))
 
 
+criterion = nn.BCELoss()
+d_optimizer = torch.optim.Adam(discriminator_model.parameters(), lr=0.0002)
+g_optimizer = torch.optim.Adam(generator_model.parameters(), lr=0.0002)
+
+
+total_step = len(train_loader)
+num_epochs = 1
+for epoch in range(num_epochs):
+    for i, (images, _) in enumerate(train_loader):
+        real_images = images.to(device)
+        real_labels = torch.ones((batch_size, 1)).to(device)
+        fake_labels = torch.zeros((batch_size, 1)).to(device)
+
+        ### train discriminator ###
+
+        # real
+        outputs = discriminator_model(real_images)
+        d_loss_real = criterion(outputs, real_labels)
+        # fake
+        latent_points = torch.rand(batch_size, 100).to(device) # latent_size = 100
+        fake_images = generator_model(latent_points)
+        outputs = discriminator_model(fake_images)
+        d_loss_fake = criterion(outputs, fake_labels)
+        # backprop and optimize
+        d_loss = d_loss_real + d_loss_fake
+        d_optimizer.zero_grad()
+        g_optimizer.zero_grad()
+        d_loss.backward()
+        d_optimizer.step()
+
+        ### train generator ###
+
+        latent_points = torch.rand(batch_size, 100).to(device)
+        fake_images = generator_model(latent_points)
+        outputs = discriminator_model(fake_images)
+        g_loss = criterion(outputs, real_labels)
+
+        d_optimizer.zero_grad()
+        g_optimizer.zero_grad()
+        g_loss.backward()
+        g_optimizer.step()
+
+        print(d_loss, g_loss)
+
+        print(i)
+
+
+        if (i % 10) == 0:
+            figure = plt.figure(figsize=(8, 8))
+            cols, rows = 3, 3
+            for i in range(1, cols * rows + 1):
+                sample_idx = torch.randint(len(fake_images), size=(1,)).item()
+                img = fake_images.detach().numpy()[sample_idx]
+                figure.add_subplot(rows, cols, i)
+                plt.axis("off")
+                plt.imshow(img.squeeze(), cmap="gray")
+            plt.show()
 
 
 
+
+
+
+
+
+
+
+### Testing Generator ###
+# latent_points = torch.rand(15, 100)
+# outputs = generator_model(latent_points)
+#
+# figure = plt.figure(figsize=(8, 8))
+# cols, rows = 3, 3
+# for i in range(1, cols * rows + 1):
+#     sample_idx = torch.randint(len(outputs), size=(1,)).item()
+#     img = outputs.detach().numpy()[sample_idx]
+#     figure.add_subplot(rows, cols, i)
+#     plt.axis("off")
+#     plt.imshow(img.squeeze(), cmap="gray")
+# plt.show()
+
+
+### Testing Discriminator ###
 # # loss and optimizer
 # criterion = nn.BCELoss()
 # optimizer = torch.optim.Adam(discriminator_model.parameters(), lr=0.0002)
