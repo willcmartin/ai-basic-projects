8652126069adf9fc845aee73df6013ff50bdee9d
Will Martin <wcmartin01@gmail.com>
2021-12-19 20:30:51 -0600

    model and trainer fixed

--- GAN/MNIST_DCGAN.py
+++ GAN/MNIST_DCGAN.py
@@ -4,14 +4,24 @@ from torchvision import datasets, transforms
 import numpy as np
 import matplotlib.pyplot as plt
 from torchsummary import summary
+from tqdm import tqdm
+
+# https://arxiv.org/pdf/1511.06434.pdf
+
+# hyperparameters
+num_epochs = 5
+latent_size = 100
+batch_size = 100
+lr = 0.0002
+beta_1= 0.5
+beta_2 = 0.999
 
 # device config
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 # get dataset
-trans = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (1.0,))])
+trans = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
 train_set = datasets.MNIST(root='./data', train=True, transform=trans, download=True)
-test_set = datasets.MNIST(root='./data', train=False, transform=trans, download=True)
 
 # visualize data
 def visualize_data():
@@ -27,185 +37,151 @@ def visualize_data():
     plt.show()
 
 # create data loaders
-batch_size = 100
-
 train_loader = torch.utils.data.DataLoader(
-                 dataset=train_set,
-                 batch_size=batch_size,
-                 shuffle=True)
-test_loader = torch.utils.data.DataLoader(
-                dataset=test_set,
-                batch_size=batch_size,
-                shuffle=False)
-
-# print("Train size: {}".format(train_loader.dataset.data.size()))
-# print("Test size: {}".format(test_loader.dataset.data.size()))
+    dataset=train_set,
+    batch_size=batch_size,
+    shuffle=True
+)
 
 # discriminator model
 class Discriminator(nn.Module):
     def __init__(self):
         super(Discriminator, self).__init__()
-        self.layer1 = nn.Sequential(
-            nn.Conv2d(1, 64, kernel_size=3, stride=2, padding=1),
-            nn.LeakyReLU(0.2),
-            nn.Dropout(0.4))
-        self.layer2 = nn.Sequential(
-            nn.Conv2d(64, 64, kernel_size=3, stride=2, padding=1),
-            nn.LeakyReLU(0.2),
-            nn.Dropout(0.4),
-            nn.Flatten())
-        self.fc = nn.Sequential(
-            nn.Linear(7*7*64, 1),
-            nn.Sigmoid())
+        self.network = nn.Sequential(
+            nn.Conv2d(1, 32, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
 
-    def forward(self, x):
-        out = self.layer1(x)
-        out = self.layer2(out)
-        out = self.fc(out)
-        return out
+            nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            nn.Conv2d(128, 1, kernel_size=4, stride=1, padding=0, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, input):
+        output = self.network(input)
+        return output.view(-1, 1).squeeze(1)
 
 discriminator_model = Discriminator().to(device)
 print("\nDiscriminator Model:")
 print(summary(discriminator_model, (1, 28, 28)))
 
-
+# generator model
 class Generator(nn.Module):
     def __init__(self):
-        super(Generator, self).__init__()
-        self.fc = nn.Sequential(
-            nn.Linear(100, 7*7*128),
-            nn.LeakyReLU(0.2))
-        self.layer1 = nn.Sequential(
-            nn.ConvTranspose2d(128, 128, kernel_size=4, stride=2, padding=1),
-            nn.LeakyReLU(0.2))
-        self.layer2 = nn.Sequential(
-            nn.ConvTranspose2d(128, 128, kernel_size=4, stride=2, padding=1),
-            nn.LeakyReLU(0.2),
-            nn.Conv2d(128, 1, kernel_size=7, padding=3),
-            nn.Sigmoid())
+      super(Generator, self).__init__()
+      self.network = nn.Sequential(
+          nn.ConvTranspose2d(latent_size, 128, kernel_size=4, stride=1, padding=0, bias=False),
+          nn.BatchNorm2d(128),
+          nn.ReLU(True),
 
-    def forward(self, x):
-        out = self.fc(x)
-        out = out.reshape(out.size(0), 128, 7, 7)
-        out = self.layer1(out)
-        out = self.layer2(out)
-        return out
+          nn.ConvTranspose2d(128, 64, kernel_size=3, stride=2, padding=1, bias=False),
+          nn.BatchNorm2d(64),
+          nn.ReLU(True),
+
+          nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2, padding=1, bias=False),
+          nn.BatchNorm2d(32),
+          nn.ReLU(True),
+
+          nn.ConvTranspose2d(32, 1, kernel_size=4, stride=2, padding=1, bias=False),
+          nn.Tanh()
+      )
+
+    def forward(self, input):
+      output = self.network(input)
+      return output
 
 generator_model = Generator().to(device)
 print("\nGenerator Model:")
-print(summary(generator_model, (1, 100)))
+print(summary(generator_model, (latent_size, 1, 1)))
+print("\n")
 
+# initialize weights
+# https://pytorch.org/tutorials/beginner/dcgan_faces_tutorial.html
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0)
 
+generator_model.apply(weights_init)
+discriminator_model.apply(weights_init)
+
+# define loss function and optimizers
 criterion = nn.BCELoss()
-d_optimizer = torch.optim.Adam(discriminator_model.parameters(), lr=0.0002)
-g_optimizer = torch.optim.Adam(generator_model.parameters(), lr=0.0002)
+d_optimizer = torch.optim.Adam(discriminator_model.parameters(), lr=lr, betas=(beta_1, beta_2))
+g_optimizer = torch.optim.Adam(generator_model.parameters(), lr=lr, betas=(beta_1, beta_2))
 
+d_losses = []
+g_losses = []
 
+# train
 total_step = len(train_loader)
-num_epochs = 1
 for epoch in range(num_epochs):
-    for i, (images, _) in enumerate(train_loader):
+    epoch_g_loss = 0
+    epoch_d_loss = 0
+    for i, (images, _) in enumerate(tqdm(train_loader, desc="Epoch: {}".format(epoch))):
+        # load images and labels
         real_images = images.to(device)
-        real_labels = torch.ones((batch_size, 1)).to(device)
-        fake_labels = torch.zeros((batch_size, 1)).to(device)
+        real_labels = torch.ones((batch_size)).to(device)
+        fake_labels = torch.zeros((batch_size)).to(device)
 
-        ### train discriminator ###
-
-        # real
+        # train discriminator
         outputs = discriminator_model(real_images)
         d_loss_real = criterion(outputs, real_labels)
-        # fake
-        latent_points = torch.rand(batch_size, 100).to(device) # latent_size = 100
+
+        latent_points = torch.rand(batch_size, latent_size, 1, 1).to(device)
         fake_images = generator_model(latent_points)
         outputs = discriminator_model(fake_images)
         d_loss_fake = criterion(outputs, fake_labels)
-        # backprop and optimize
-        d_loss = d_loss_real + d_loss_fake
+
         d_optimizer.zero_grad()
-        g_optimizer.zero_grad()
-        d_loss.backward()
+        d_loss_real.backward()
+        d_loss_fake.backward()
         d_optimizer.step()
 
-        ### train generator ###
+        epoch_d_loss += d_loss_real + d_loss_fake
 
-        latent_points = torch.rand(batch_size, 100).to(device)
+        # train generator
+        latent_points = torch.rand(batch_size, latent_size, 1, 1).to(device)
         fake_images = generator_model(latent_points)
         outputs = discriminator_model(fake_images)
         g_loss = criterion(outputs, real_labels)
 
-        d_optimizer.zero_grad()
         g_optimizer.zero_grad()
         g_loss.backward()
         g_optimizer.step()
 
-        print(d_loss, g_loss)
+        epoch_g_loss += g_loss
 
-        print(i)
+    d_losses.append(epoch_d_loss / i)
+    g_losses.append(epoch_g_loss / i)
 
+# plot losses
+plt.figure()
+plt.plot(d_losses, label='Discriminator loss')
+plt.plot(g_losses, label='Generator Loss')
+plt.xlabel("Epoch")
+plt.ylabel("Loss")
+plt.legend()
+plt.show()
 
-        if (i % 10) == 0:
-            figure = plt.figure(figsize=(8, 8))
-            cols, rows = 3, 3
-            for i in range(1, cols * rows + 1):
-                sample_idx = torch.randint(len(fake_images), size=(1,)).item()
-                img = fake_images.detach().numpy()[sample_idx]
-                figure.add_subplot(rows, cols, i)
-                plt.axis("off")
-                plt.imshow(img.squeeze(), cmap="gray")
-            plt.show()
-
-
-
-
-
-
-
-
-
-
-### Testing Generator ###
-# latent_points = torch.rand(15, 100)
-# outputs = generator_model(latent_points)
-#
-# figure = plt.figure(figsize=(8, 8))
-# cols, rows = 3, 3
-# for i in range(1, cols * rows + 1):
-#     sample_idx = torch.randint(len(outputs), size=(1,)).item()
-#     img = outputs.detach().numpy()[sample_idx]
-#     figure.add_subplot(rows, cols, i)
-#     plt.axis("off")
-#     plt.imshow(img.squeeze(), cmap="gray")
-# plt.show()
-
-
-### Testing Discriminator ###
-# # loss and optimizer
-# criterion = nn.BCELoss()
-# optimizer = torch.optim.Adam(discriminator_model.parameters(), lr=0.0002)
-#
-# # train the discriminator model on half fake, half real data
-# total_step = len(train_loader)
-# num_epochs = 1
-# for epoch in range(num_epochs):
-#     for i, (images, labels) in enumerate(train_loader):
-#         if i % 2 == 0:
-#             images = torch.rand(100, 1, 28, 28)
-#             labels = torch.zeros((100,1))
-#         else:
-#             labels = torch.ones((100,1))
-#
-#         images = images.to(device)
-#         labels = labels.to(device)
-#
-#         # forward pass
-#         outputs = discriminator_model(images)
-#         loss = criterion(outputs, labels)
-#
-#         # backward and optimize
-#         optimizer.zero_grad()
-#         loss.backward()
-#         optimizer.step()
-#
-#         if (i+1) % 10 == 0:
-#             print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
-#                    .format(epoch+1, num_epochs, i+1, total_step, loss.item()))
+# plot sample generated images
+latent_points = torch.rand(batch_size, latent_size, 1, 1).to(device)
+fake_images = generator_model(latent_points)
+figure = plt.figure(figsize=(8, 8))
+cols, rows = 3, 3
+for i in range(1, cols * rows + 1):
+    sample_idx = torch.randint(len(fake_images), size=(1,)).item()
+    img = fake_images.cpu().detach().numpy()[sample_idx]
+    figure.add_subplot(rows, cols, i)
+    plt.axis("off")
+    plt.imshow(img.squeeze(), cmap="gray")
+plt.show()
