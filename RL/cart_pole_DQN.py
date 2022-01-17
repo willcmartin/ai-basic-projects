8be09a7db6a224d8e48649d197b0b565ad6c8339
Will Martin <wcmartin01@gmail.com>
2022-01-17 17:27:18 -0600

    inital commit

---
+++ RL/cart_pole_DQN.py
@@ -0,0 +1,167 @@
+# working through pytorch cart pole DQN tutorial:
+# https://pytorch.org/tutorials/intermediate/reinforcement_q_learning.html
+
+# enviornment: cart pole
+# reward: +1 per timesetep
+# termination state: pole falls over or cart > 2.4 units from center
+# input: diff between current enviornment image and prev
+
+import gym
+import math
+import random
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from collections import namedtuple, deque
+from itertools import count
+from PIL import Image
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torchvision.transforms as T
+
+env = gym.make('CartPole-v0').unwrapped
+
+# set up matplotlib for iPython
+is_ipython = 'inline' in matplotlib.get_backend()
+if is_ipython:
+    from IPython import display
+
+# interactive mode
+plt.ion()
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# replay memory
+# stores transitions, randomly reuse transitions to decorrelate batch
+
+Transition = namedtuple('Transition', ('state', 'action', 'next_state', 'reward'))
+
+class ReplayMemory:
+    def __init__(self, capacity):
+        self.memory = deque([], maxlen=capacity)
+
+    def push(self, *args):
+        self.memory.append(Transition(*args))
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
+
+# DQN (Deep Q Learning)
+# maximize cumulative reward (e.g. return)
+# discount (0-1) ensures sum converges, weighted so that rewards from near future are more signification
+# policy (state) = argmax Q(state, action)
+# we want to find Q function: find temporal difference error
+# Huber loss (mean squared error when error small and mean absolute error when error is large) robust to outliers
+#   calculated from transistion batch from replay memory
+
+# Q-network
+# input: state (s) (diff between two image frames)
+# ouputs: Q(s, left) Q(s, right)
+# predict expected return given action left or right with the current state (s)
+
+class DQN(nn.Module):
+    def __init__(self, h, w, outputs):
+        super(DQN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=5, stride=2)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=5, stride=2)
+        self.bn2 = nn.BatchNorm2d(32)
+        self.conv3 = nn.Conv2d(32, 32, kernel_size=5, stride=2)
+        self.bn3 = nn.BatchNorm2d(32)
+
+        # compute linear layer input size based on input image size
+        def conv2d_size_out(size, kernal_size=5, stride=2):
+            return (size - (kernal_size - 1) - 1) // stride + 1
+
+        convw = conv2d_size_out(conv2d_size_out(conv2d_size_out(w)))
+        convh = conv2d_size_out(conv2d_size_out(conv2d_size_out(h)))
+        linear_input_size = convw * convh * 32
+
+        self.head = nn.Linear(linear_input_size, outputs)
+
+    def forward(self, x):
+        x = x.to(device)
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
+
+        return self.head(x.view(x.size(0), -1))
+
+# input extraction: process images from environment
+resize = T.Compose([T.ToPILImage(),
+                    T.Resize(40, interpolation=Image.CUBIC),
+                    T.ToTensor()])
+
+
+def get_cart_location(screen_width):
+    world_width = env.x_threshold * 2
+    scale = screen_width / world_width
+    return int(env.state[0] * scale + screen_width / 2.0)  # MIDDLE OF CART
+
+def get_screen():
+    # Returned screen requested by gym is 400x600x3, but is sometimes larger
+    # such as 800x1200x3. Transpose it into torch order (CHW).
+    screen = env.render(mode='rgb_array').transpose((2, 0, 1))
+    # Cart is in the lower half, so strip off the top and bottom of the screen
+    _, screen_height, screen_width = screen.shape
+    screen = screen[:, int(screen_height*0.4):int(screen_height * 0.8)]
+    view_width = int(screen_width * 0.6)
+    cart_location = get_cart_location(screen_width)
+    if cart_location < view_width // 2:
+        slice_range = slice(view_width)
+    elif cart_location > (screen_width - view_width // 2):
+        slice_range = slice(-view_width, None)
+    else:
+        slice_range = slice(cart_location - view_width // 2,
+                            cart_location + view_width // 2)
+    # Strip off the edges, so that we have a square image centered on a cart
+    screen = screen[:, :, slice_range]
+    # Convert to float, rescale, convert to torch tensor
+    # (this doesn't require a copy)
+    screen = np.ascontiguousarray(screen, dtype=np.float32) / 255
+    screen = torch.from_numpy(screen)
+    # Resize, and add a batch dimension (BCHW)
+    return resize(screen).unsqueeze(0)
+
+# show sample frame
+
+env.reset()
+# plt.figure()
+# plt.imshow(get_screen().cpu().squeeze(0).permute(1, 2, 0).numpy(),
+#            interpolation='none')
+# plt.title('Example extracted screen')
+# plt.show(block=True)
+
+
+# training
+
+# hyperparameters
+BATCH_SIZE = 128
+GAMMA = 0.999
+EPS_START = 0.9
+EPS_END = 0.05
+EPS_DECAY = 200
+TARGET_UPDATE = 10
+
+# get screen size
+init_screen = get_screen()
+_, _, screen_height, screen_width = init_screen.shape
+
+# num actions in enviornment
+n_actions = env.action_space.n
+
+policy_net = DQN(screen_height, screen_width, n_actions).to(device)
+target_net  = DQN(screen_height, screen_width, n_actions).to(device)
+target_net.load_state_dict(policy_net.state_dict())
+target_net.eval()
+
+optimizer = optim.RMSprop(policy_net.parameters())
+memory = ReplayMemory(10000)
+
+steps_done = 0
